cace996d42883ad598fa402dfc37dbde100bba4b
langep <patrick.l.lange@gmail.com>
2017-10-05 16:39:17 -0700

    Added check_dir and check_file

--- utils.sh
+++ utils.sh
@@ -18,6 +18,24 @@ check_null_or_unset() {
 	local readonly var_val=${1}
 	local readonly var_name=${2}
 	if [[ -z ${var_val:+x} ]] ; then
-		fatal "${var_name} is null or unset. Must be set."
+		fatal "${var_name} is null or unset."
+	fi
+}
+
+check_dir() {
+	local readonly path=${1}
+	local readonly var_name=${2}
+	check_null_or_unset ${path} ${var_name}
+	if [[ ! -d ${path} ]] ; then
+		fatal "${var_name} is not a directory."
+	fi
+}
+
+check_file() {
+	local readonly path=${1}
+	local readonly var_name=${2}
+	check_null_or_unset ${path} ${var_name}
+	if [[ ! -f ${path} ]] ; then
+		fatal "${var_name} is not a file."
 	fi
 }
